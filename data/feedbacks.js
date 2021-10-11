bb8eac6d32c1a9ab98793d79de5ef0ad4a399215
Natacha Romano <natacharomanonr@gmail.com>
2021-10-11 13:32:19 -0300

    updating feedbacks

--- data/feedbacks.js
+++ data/feedbacks.js
@@ -4,6 +4,10 @@ const feedbacks = [
         date: 'Aug 16, 2020 - Oct 27, 2020'
     },
     {
+        text: "Not only did Natacha provide an exceptional Frontend, but she also helped us 'beautify' certain parts of the UX Design. Apart from CSS and HTML she is very proefficient with Javascript/React which helped us add interactive elements to the website. She's available almost 24/7 and is a very pleasant person to work with. Not only did she stick to the deadline, but she delivered way faster than the initial expectations. She's very patient and looks at every detail of the UX. People in UX say that 'Pixel Perfect' Designs are not possible. Natacha proved them wrong. Highly recommended!", 
+        date: 'Aug 10, 2021 - Oct 10, 2021'
+    },
+    {
         text: "Natacha's skill with core CSS and React is clear and enabled rapid turnaround and high quality work on our website development project. More than that, she quickly learned new tools in the Gatsby framework to enable us to optimize site load times and integrate the Ghost blog as a content source for our site. She was responsive, her communication was clear, and she was easy to work with and actively participated where appropriate to maintain project issue tracking in Gitlab.",
         date: 'Oct 19, 2020 - Nov 25, 2020'
     },
@@ -14,6 +18,10 @@ const feedbacks = [
     {
         text: "Natacha did a great job! Delivered our request on time and done extremely well the first time. We only had a few small questions and she quickly got back to us and provided screencasts going over the parts we had questions about. I also appreciate that she asked all needed questions at the beginning to clarify exactly what was needed. After we answered them, she understood perfectly what we needed and went to work. It's amazing to find someone you can trust to go off and do great work and we will definitely be trusting more HTML project to her in the future.",
         date: 'Jul 21, 2020 - Jul 29, 2020',
+    },
+    {
+        text: "Natacha did exactly what was requested for the task and delivered in a timely fashion. There was excellent communication throughout, asking relevant questions and responses were always prompt and thoughtful. Would recommend!",
+        date: 'Jul 21, 2021 - Jul 22, 2021',
     },
     {
         text: "Natacha’s new to the platform but has a ton of skills that I enjoyed during the contract like knowledge, timeliness and overall great attitude towards working toward the goal. She was communicative and followed through on everything she agreed to. A+ work here will definitely hire again!",
